e62d022c6af2a3cdb4b8c0c74ea3f51dbace290e
sirliu <sirliu110@gmail.com>
2022-03-11 13:04:57 +0800

    优化逻辑，宿主机映射目录中有文件时容器内不再进行复制操作。

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-cp -ra /datas/* /config
+if [ "`ls -A /config`" = "" ];
+then 
+        echo "检测到 /config 目录为空，准备从 /datas 目录复制初始文件"
+        cp -ra /datas/* /config
+fi
 rm -r /datas
 ln -s /config /datas
+
 nohup ss-local -c /config/config.json --acl /config/bypass-lan-china.acl > /config/ss-local.log 2>&1 &
 privoxy --no-daemon /config/privoxy-config
